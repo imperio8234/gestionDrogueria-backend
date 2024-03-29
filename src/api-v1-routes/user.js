ce418bf0f66452440da368a201e9c6c365d5b0f3
jhon anderson <garciacalvojhonanderson@gmail.com>
2024-03-29 09:42:40 -0500

    mod

--- src/api-v1-routes/user.js
+++ src/api-v1-routes/user.js
@@ -20,10 +20,4 @@ router.put("/password", recoverPassword);
 router.post("/aut", authenticateUser);
 router.post("/", createUsers);
 router.delete("/", verify, deleteUsers);
-
-const token =  [
-  'aut=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZF91c3VhcmlvIjoxMywibm9tYnJlIjoicGVkcm8iLCJhY3Rpdm8iOjEsImZlY2hhIjoiNC8zLzIwMjQiLCJpbmljaW8iOiI0Iiwibm9tYnJlTmVnb2NpbyI6bnVsbCwiY2VsdWxhciI6IjA5ODcxMjM0NTYiLCJpYXQiOjE3MDk1Nzg2MDEsImV4cCI6MTcwOTYxNDYwMX0.-qBEhaKdwGDUMKn0TkIhT_WinXYsj0nAsN-08Ru3Q8Q; Path=/; HttpOnly'
-]
-console.log(token[0].split(";")[0])
-
 module.exports = router;
