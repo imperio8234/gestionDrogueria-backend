bd094c05779ae0251d6e2f235cb743b17cbf940a
jhon anderson <garciacalvojhonanderson@gmail.com>
2024-03-29 17:42:35 -0500

    cambio de cors

--- src/toolsDev/midelware/bd_conection.js
+++ src/toolsDev/midelware/bd_conection.js
@@ -1,7 +1,7 @@
 const sql = require("mysql2");
 
 // avariables
-const DB_HOST = process.env.DB_HOST || "146.190.149.118";
+const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_USER = process.env.DB_USER || "root";
 const DB_PASSWORD = process.env.DB_PASSWORD || "imperio8234";
 const DB_NAME = process.env.DB_NAME || "villa";
@@ -10,7 +10,7 @@ const DB_PORT = process.env.DB_PORT || 3306;
 const conexion = sql.createConnection({
   host: DB_HOST,
   user: DB_USER,
-  password: "",
+  password: DB_PASSWORD,
   port: DB_PORT,
   database: DB_NAME
 });
