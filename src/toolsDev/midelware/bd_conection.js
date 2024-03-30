4338d992daf55c94a2dc5c18ad6f836f1e1b0236
jhon anderson <garciacalvojhonanderson@gmail.com>
2024-03-30 16:19:04 -0500

    modif

--- src/toolsDev/midelware/bd_conection.js
+++ src/toolsDev/midelware/bd_conection.js
@@ -2,7 +2,7 @@ const sql = require("mysql2");
 
 // avariables
 const DB_HOST = process.env.DB_HOST || "localhost";
-const DB_USER = process.env.DB_USER || "root";
+const DB_USER = process.env.DB_USER || "villa";
 const DB_PASSWORD = process.env.DB_PASSWORD || "imperio8234";
 const DB_NAME = process.env.DB_NAME || "villa";
 const DB_PORT = process.env.DB_PORT || 3306;
