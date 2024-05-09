068a3db61ca1be5b3cfbe24b833a55601607bc09
jhon anderson <garciacalvojhonanderson@gmail.com>
2024-05-09 12:36:51 -0500

    correcion de error de verificacion

--- src/toolsDev/useVerIfyDate.js
+++ src/toolsDev/useVerIfyDate.js
@@ -1,13 +1,21 @@
 const useverifyDate = (date) => {
+  // Configurar la zona horaria a Colombia
+  const opcionesFecha = { timeZone: 'America/Bogota' };
+
+  // Obtener la fecha actual en la zona horaria de Colombia
+  const fechaActual = new Date();
+  const fechaActualColombia = new Date(fechaActual.toLocaleString('en-US', opcionesFecha));
+
+  // Configurar la fecha de registro y la duración de la suscripción
   const fechaRegistro = new Date(date);
   const duracionSuscripcion = 30;
 
-  const fechaActual = new Date();
-
+  // Obtener la fecha de vencimiento sumando la duración de la suscripción a la fecha de registro
   const fechaVencimiento = new Date(fechaRegistro);
   fechaVencimiento.setDate(fechaRegistro.getDate() + duracionSuscripcion);
 
-  const diasRestantes = Math.ceil((fechaVencimiento - fechaActual) / (1000 * 60 * 60 * 24));
+  // Calcular los días restantes
+  const diasRestantes = Math.ceil((fechaVencimiento - fechaActualColombia) / (1000 * 60 * 60 * 24));
   return diasRestantes;
 };
 module.exports = useverifyDate;
